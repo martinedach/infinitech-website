1d906be6aa7bcfc6e8436b74c133508f613df954
Martin Mathew <martinedacheril@icloud.com>
2024-12-02 16:20:53 +1300

    minor changes

--- app/routers/sitemap_router.py
+++ app/routers/sitemap_router.py
@@ -13,7 +13,7 @@ categories = ["computers", "laptops", "wifi"]
 
 @router.get("/sitemap.xml", response_class=Response)
 async def sitemap():
-    base_url = "http://127.0.0.1:8000"
+    base_url = "https://www.infinitech.com"
 
     # Generate XML sitemap
     xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
